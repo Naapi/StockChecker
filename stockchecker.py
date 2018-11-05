1d261cfecfe7bcf57a0c7f61fdd20e33f6c700da
Terho <35961163+Naapi@users.noreply.github.com>
2018-11-05 15:58:08 +0200

    brackets
    changed brackets around url

--- stockchecker.py
+++ stockchecker.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(c)
 sou = BeutufuulSoup(c)
-url = 'https://finance.yahoo.com/quote/AAPL/'
+url = ('https://finance.yahoo.com/quote/AAPL/')
 data = requests.get(url)
 jsoni = data.json()
 print(data.text)
